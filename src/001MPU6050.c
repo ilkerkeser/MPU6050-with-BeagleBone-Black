56c7b7288eafdf22bebabeded7bf46c4e9278af0
İlker KESER <58569512+ilkerkeser@users.noreply.github.com>
2025-08-09 22:19:14 +0300

    Update 001MPU6050.c

--- src/001MPU6050.c
+++ src/001MPU6050.c
@@ -4,7 +4,7 @@
  Author      : İK
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description :
  ============================================================================
  */
 
